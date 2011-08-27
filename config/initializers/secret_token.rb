22558740264a032aebea3186cbac98026f0c63ff
Carter Cleveland <carter@artsymail.com>
2011-08-27 16:27:50 -0400

    updated readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whereto::Application.config.secret_token = '1a7f48479318fc4181b92e4069955ac727b09a8bdd3e5cdfaad9d5cc236a3c864347890ff65c580872c3f8787c0bc430a21aa185d04d90feaf049e9d3b349539'
